c13a7f266b2a362257b435e871bbea306ff8d6a9
An Do Eon <andoune0719@gmail.com>
2020-09-21 14:52:34 +0900

    social links update

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -5,21 +5,30 @@ import { socialMediaLinks } from "../../portfolio";
 function Social() {
 	return(
 		<div className="social-media-links">
-			<a href={socialMediaLinks.github} className="icon-button github" target="_blank" rel="noopener noreferrer">
+			{socialMediaLinks.github && <a href={socialMediaLinks.github} className="icon-button github" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-github"></i>
-			</a>
-			<a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank" rel="noopener noreferrer">
+			</a>}
+			{socialMediaLinks.linkedin && <a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-linkedin"></i>
-			</a>
-			<a href={`mailto:${socialMediaLinks.email}`} className="icon-button email" target="_blank" rel="noopener noreferrer">
+			</a>}
+			{socialMediaLinks.email && <a href={`mailto:${socialMediaLinks.email}`} className="icon-button email" target="_blank" rel="noopener noreferrer">
 				<i className="fas fa-envelope"></i>
-			</a>
-			<a href={socialMediaLinks.facebook} className="icon-button facebook" target="_blank" rel="noopener noreferrer">
+			</a>}
+			{socialMediaLinks.facebook && <a href={socialMediaLinks.facebook} className="icon-button facebook" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-facebook"></i>
-			</a>
-			<a href={socialMediaLinks.medium} className="icon-button medium" target="_blank" rel="noopener noreferrer">
+			</a>}
+			{socialMediaLinks.twitter && <a href={socialMediaLinks.twitter} className="icon-button twitter" target="_blank" rel="noopener noreferrer">
+				<i className="fab fa-twitter"></i>
+			</a>}
+			{socialMediaLinks.instagram && <a href={socialMediaLinks.instagram} className="icon-button instagram" target="_blank" rel="noopener noreferrer">
+				<i className="fab fa-instagram"></i>
+			</a>}
+			{socialMediaLinks.medium && <a href={socialMediaLinks.medium} className="icon-button medium" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-medium"></i>
-			</a>
+			</a>}
+			{socialMediaLinks.stackoverflow && <a href={socialMediaLinks.stackoverflow} className="icon-button stackoverflow" target="_blank" rel="noopener noreferrer">
+				<i className="fab fa-stack-overflow"></i>
+			</a>}
 		</div>
 	);
 }
