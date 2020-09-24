86a36cad0ea45e592f733bb58ea8cb9e9c485bd8
An Do Eon <andoune0719@gmail.com>
2020-09-24 21:32:35 +0900

    fix warning: word 'photo' is unnecessary

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -15,7 +15,7 @@ function Contact() {
             <div className="inner">
                 
                 <div className="profile-img">
-                    <img src={require("../../assets/your photo.png")} alt="profile photo" />
+                    <img src={require("../../assets/your photo.png")} alt="profile" />
                 </div>
                 <div className="wrapper-inner">
                     <div className="contact-subtitle">
